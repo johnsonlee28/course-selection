98dccc1037f6aaf9644adcb35bbdde8eec8a33a1
Johnsonlee <johnsonlee28@gmail.com>
2017-03-31 08:41:07 +0800

    Section03

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,6 @@
 class Course < ApplicationRecord
   has_many :coz_relationships
   has_many :students, through: :coz_relationships, source: :student
+
+  belongs_to :teacher
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,4 +1,6 @@
 class Teacher < ApplicationRecord
   has_many :edu_relationships
   has_many :students, through: :edu_relationships, source: :student
+
+  has_many :courses, dependent: :destroy
 end
